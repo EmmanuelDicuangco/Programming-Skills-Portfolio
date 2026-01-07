6d13f0fc67d1558a5714601004de6ad075fa6cac
EmmanuelDicuangco <dicuangcoemmanuel@gmail.com>
2026-01-07 21:35:54 +0300

    I add the advanced requirements in which i add a leap year for February that has 29 days in ordinary years that has 28 days

--- Exercise 5 (Days of the Month).py
+++ Exercise 5 (Days of the Month).py
@@ -17,4 +17,36 @@ month = int(input('Enter a Month Number (1-12). '))
 if month in months:
   print(f'This month has {months[month]} days.')
 else:
+  print('Invalid Month Number.')
+
+--------------------------------------------------------
+
+# Advanced Requirements
+
+months = {
+  1: 31,
+  2: 28,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31
+}
+
+month = int(input('Enter a Month Number (1-12). '))
+if month in months:
+  if month == 2:
+    leap_year = input ('Is it a leap[ year? (yes/no)').lower()
+    if leap_year == 'yes':
+        print('February has 29 days if it is a leap year.')
+    else:
+        print('February has 28 days if it is an ordinary year.')
+  else:   
+      print(f'This month has {months[month]} days.')
+else:
   print('Invalid Month Number')
