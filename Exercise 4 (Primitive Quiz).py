c641e7840236c259a2c16e6391dd3537dd77f088
EmmanuelDicuangco <dicuangcoemmanuel@gmail.com>
2026-01-07 18:35:02 +0300

    I create a simple program that ask a user question, evaluates their answer, and  provides feedback if the user answer is wrong or correct

---
+++ Exercise 4 (Primitive Quiz).py
@@ -0,0 +1,6 @@
+answer = input('What is the capital of France?')
+
+if answer == 'Paris':
+    print('Your answer is correct, which is Paris')
+else:
+    print('Your answer is wrong. Correct answer is Paris')
