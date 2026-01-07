64c516c6d7d3cc37ea0d1ed7144de51c183f0326
EmmanuelDicuangco <dicuangcoemmanuel@gmail.com>
2026-01-07 18:25:00 +0300

    I create program in where it will print my name, age, and my hometown using a dictionary. Then i use this specific block of code (sep='\n') when run it will print in different line like a bullet point way
    sep - is  a seperator between values
    \n - means new line
    sep='\n' - when combined it will print each value in a different line

---
+++ Exercise 3 (Biography).py
@@ -0,0 +1,11 @@
+name = 'Emmanuel Joshua A Dicuangco'
+hometown = 'Manila Valenzuela Philippines'
+age = 19
+
+my_info = {
+  'Name': name,
+  'Hometown': hometown,
+  'Age': age
+}
+
+print(my_info['Name'], my_info['Hometown'], my_info['Age'], sep='\n')
