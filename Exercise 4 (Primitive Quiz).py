13c4fb120a9e1fe99cd7b4e4c9d3b5c8f475af99
EmmanuelDicuangco <dicuangcoemmanuel@gmail.com>
2026-01-07 21:24:51 +0300

    I add the advanced requirements for this exercise
    in which i extend the country capital quiz and add this function .lower so the user can type caps, small letters or both.

--- Exercise 4 (Primitive Quiz).py
+++ Exercise 4 (Primitive Quiz).py
@@ -4,3 +4,29 @@ if answer == 'Paris':
     print('Your answer is correct, which is Paris')
 else:
     print('Your answer is wrong. Correct answer is Paris')
+
+
+-------------------------------------------------------------------
+
+# Advanced Requirements
+
+capitals = {
+    'France': 'Paris',
+    'Belgium': 'Brussels',
+    'Russia': 'Moscow',
+    'Spain': 'Madrid',
+    'United Kingdom': 'London',
+    'Turkey': 'Ankara',
+    'Norway': 'Oslo',
+    'Netherlands': 'Amsterdam',
+    'Romania': 'Bucharest',
+    'Ukraine': 'Klev'
+}
+
+for country, capital in capitals.items():
+    answer = input(f'What is the capital of this {country}?')
+
+    if answer.lower() == capital.lower():
+        print('Correct')
+    else:
+        (f'Wrong. The correct answer is {capital}.')
