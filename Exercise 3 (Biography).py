b919d125ebe676eb4f2026d016be6222b92d3361
EmmanuelDicuangco <dicuangcoemmanuel@gmail.com>
2026-01-07 20:55:07 +0300

    I did the advanced requirement in which you can type your name, age, and your hometown i use the function isdigit() and while loop to confirm the input before it convert to an integer

--- Exercise 3 (Biography).py
+++ Exercise 3 (Biography).py
@@ -9,3 +9,25 @@ my_info = {
 }
 
 print(my_info['Name'], my_info['Hometown'], my_info['Age'], sep='\n')
+
+--------------------------------------------------------------------------
+# Advance Requirement
+
+name = input('Enter your full name')
+hometown = input('Enter your hometown')
+
+while True:
+  age_input = input('Enter your age')
+  if age_input.isdigit():
+    age = int(age_input)
+    break
+  else:
+      print('Please enter a valid numbe.')
+
+my_info = {
+  'Name': name,
+  'Hometown': hometown,
+  'Age': age
+}
+
+print(my_info['Name'], my_info['Hometown'], my_info['Age'], sep='\n')
